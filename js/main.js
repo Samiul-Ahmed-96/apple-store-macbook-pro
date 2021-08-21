5aacf7c3ea8669ad6274e6432aa2132e9c35d5eb
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2021-08-21 19:36:02 +0600

    create coupon

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ const storagePrice = document.getElementById('storage-cost');
 const deliveryCharge = document.getElementById('delivery-cost');
 const basePrice = document.getElementById('base-price');
 const total = document.getElementById('total-cost');
+const mainTotal = document.getElementById('total-main');
 
 function updateTotal(){
 	const baseCost = Number(basePrice.innerText);
@@ -12,6 +13,18 @@ function updateTotal(){
 	const deliveryCost = Number(deliveryCharge.innerText);
 	const grandTotal = baseCost + memoryCost + storageCost + deliveryCost;
 	total.innerText = grandTotal;
+	mainTotal.innerText = grandTotal;
+	let couponText = document.getElementById('coupon-field');
+	let couponTextValue = couponText.value;
+	if(couponTextValue == 'stevekaku'){
+		console.log(grandTotal);
+		const discountAmount = grandTotal * 20;
+		const discount = discountAmount / 100;
+		const newDiscountAmount = grandTotal - discount ;
+		mainTotal.innerText = newDiscountAmount;
+		couponText.value = '';
+		
+	}
 }
 
 document.getElementById('memory-8gb').addEventListener('click',function(){
@@ -48,4 +61,7 @@ document.getElementById('delivery-quick').addEventListener('click',function(){
 	deliveryCharge.innerText = 20;
 	updateTotal();
 	
+})
+document.getElementById('coupon-add-btn').addEventListener('click',function(){
+	updateTotal();
 })
